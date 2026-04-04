547a284cc90212d68eb92467c01d773c6652a7d8
Invoker-pray <jiaohongbao04@gmail.com>
2026-04-04 13:38:42 +0800

    Sat Apr  4 01:38:42 PM CST 2026 add: clean_onlycode.sh, for upload zip.

---
+++ clean_onlycode.sh
@@ -0,0 +1,9 @@
+rm -rf bitstream &
+hwh .git
+rm -rf sw/fw_hex_batch* sw/.venv
+rm -rf picorv32/vivado_proj
+rm -rf picorv32/fw/data picorv32/fw/small_mlp_data*
+cd picorv32/fw
+make clean
+cd ../../
+rm -rf clean_onlycode.sh
